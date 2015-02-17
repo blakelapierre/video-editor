741325a3efd4c0b06e74521943e7fc509b09f0e9
Blake La Pierre <blakelapierre@gmail.com>
2015-02-16 19:34:27 -0800

    Future directions (browserify doesn't allow dynamic requires like this; look into webpack)

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -6,4 +6,24 @@ module.exports = {
     .directive('center', require('./modules/base-angular/center/directive'))
     .directive('helloWorld', require('./modules/base-angular/helloWorld/directive'))
     .directive('spinner',    require('./modules/base-angular/spinner/directive'))
+
+  // 'base-angular': define('base-angular', [], {
+  //   directives: [
+  //     'center',
+  //     'helloWorld',
+  //     'spinner'
+  //   ]
+  // })
 };
+
+// function define(name, dependencies, components) {
+//   const module = angular.module(name, dependencies),
+//         {directives} = components;
+
+//   directives.forEach(directive => {
+//     console.log(directive);
+//     module.directive(directive, require(`./modules/${name}/${directive}/directive`));
+//   });
+
+//   return module;
+// }
